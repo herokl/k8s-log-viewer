54008e4d1ff0b1278ac3ecf1bcb7b019e896a3d0
lmk <1272837619@qq.com>
2025-08-07 18:40:27 +0800

    工作流

--- src/main/java/com/longfor/lmk/k8slogviewer/K8sLogViewer.java
+++ src/main/java/com/longfor/lmk/k8slogviewer/K8sLogViewer.java
@@ -2,13 +2,13 @@ package com.longfor.lmk.k8slogviewer;
 
 import com.longfor.lmk.k8slogviewer.config.AppConfig;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Objects;
 
 public class K8sLogViewer extends Application {
@@ -16,6 +16,11 @@ public class K8sLogViewer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         AppConfig.setMainStage(primaryStage);
+        // ✨ 在这里添加以下代码来处理窗口关闭事件
+        primaryStage.setOnCloseRequest(event -> {
+            Platform.exit(); // 优雅地关闭 JavaFX 平台
+            System.exit(0);  // 终止 JVM
+        });
         // 加载 FXML 文件
         FXMLLoader fxmlLoader = new FXMLLoader(K8sLogViewer.class.getResource("/com/longfor/lmk/k8slogviewer/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
@@ -27,18 +32,6 @@ public class K8sLogViewer extends Application {
     }
 
     public static void main(String[] args) {
-        try {
-            launch(args);
-        } catch (Exception e) {
-            writeErrorToFile(e); // 写入日志
-        }
-    }
-
-    private static void writeErrorToFile(Exception e) {
-        try (PrintWriter writer = new PrintWriter("error.log")) {
-            e.printStackTrace(writer);
-        } catch (IOException ioException) {
-            ioException.fillInStackTrace();
-        }
+        launch(args);
     }
 }
